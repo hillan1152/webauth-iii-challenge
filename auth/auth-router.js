c1f677b25d84400e99f974421d7562f14d481329
Anthony Hill <hillan1152@gmail.com>
2019-12-18 18:09:20 -0500

    created login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -12,8 +12,6 @@ router.post('/register', (req, res) => {
     const hash = bcrypt.hashSync(user.password, 10);
     user.password = hash;
 
-    console.log(user)
-
     Users.add(user)
         .then(saved => {
             res.status(201).json(saved);
@@ -21,7 +19,49 @@ router.post('/register', (req, res) => {
         .catch(error => {
             res.status(500).json(error);
       });
+});
+
+// LOGIN WITH JWT
+router.post('/login', (req, res) => {
+    let { username, password } = req.body
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)){
+                // CREATES TOKEN
+                const token = makeToken(user);
+
+                res.status(200).json({
+                    token,
+                    message: "Welcome!"
+                })
+            } else {
+                res.status(401).json({ message: 'You shall not pass!' })
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+          });
 })
+
+
+
+
+
+// Create Login Token
+function makeToken(user){
+    const payload = {
+        username: user.username,
+        department: user.department
+    };
+    const secret = process.env.JWT_SECRET || "This is a secret";
+
+    const options = {
+        expiresIn: "1h",
+    }
+    return jwt.sign(payload, secret, options);
+}
 
 
 module.exports = router;
