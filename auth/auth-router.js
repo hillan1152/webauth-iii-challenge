84a3a9ed55b1ab3cb56faffe12015d43d9243126
Anthony Hill <hillan1152@gmail.com>
2019-12-18 17:47:17 -0500

    Registration

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -11,6 +11,16 @@ router.post('/register', (req, res) => {
     let user = req.body;
     const hash = bcrypt.hashSync(user.password, 10);
     user.password = hash;
+
+    console.log(user)
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+      })
+        .catch(error => {
+            res.status(500).json(error);
+      });
 })
 
 
